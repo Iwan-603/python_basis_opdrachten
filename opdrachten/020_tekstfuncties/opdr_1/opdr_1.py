f5354c04cd915febd42654812db666bb3225c4cc
Iwan-603 <97087899@st.deltion.nl>
2024-11-21 15:39:01 +0100

    Opdracht 2

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -8,12 +8,18 @@
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
 
-print()
+zin = "O mamma mia, wat maak je toch heerlijke pizza's"
+print(zin)
 
 # Gebruik onderstaande variabelen en de print-functie
 # en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
 voornaam = "albert"
 achternaam = "einstein"
+zin2 =  "zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!"
 
 
-print()
+#print de zin met enkel eerste letter hoofdletter
+zin3 = (f"{voornaam.upper()} {achternaam.capitalize()} {zin2}")  
+print(zin3)
+
+ahh i get it
