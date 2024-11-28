004e1d965d7efc3ac7adf255554ae4c8f0af952e
Iwan-603 <97087899@st.deltion.nl>
2024-11-28 15:01:07 +0100

    OPDRACHT 1 040

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,11 +1,14 @@
 # Opdracht 1 lists
 # Naam student:
 # Groep:
+mylist = []
+dict_1 = { "id": 0, "voornaam": "jan", "achternaam": "varken" }
+dict_2 = { "id": 1, "voornaam": "simon", "achternaam": "jtrkq" }
+dict_3 = { "id": 2, "voornaam": "patrick", "achternaam": "wkjff" }
+dict_4 = { "id": 3, "voornaam": "rick", "achternaam": "diktrom" }
 
-mylist = { "id": 0, "voornaam": "", "achternaam": "" }
-dict_1 = { "id": 1, "voornaam": "jan", "achternaam": "4" }
-dict_2 = { "id": 2, "voornaam": "simon", "achternaam": "3" }
-dict_3 = { "id": 3, "voornaam": "patrick", "achternaam": "2" }
-dict_4 = { "id": 4, "voornaam": "rick", "achternaam": "diktrom" }
+mylist = []
+for d in [dict_1, dict_2, dict_3, dict_4]:
+    mylist.append(d)
 
-
+print(mylist[1]["voornaam"], mylist[1]["achternaam"])
